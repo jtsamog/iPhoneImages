5dcbb15da86772a64805137917b797d7f7f9df48
Sam Ogunyowa <mailsamog@gmail.com>
2017-07-17 11:58:24 -0400

    Initial Commit

---
+++ iPhoneImages/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iPhoneImages
+//
+//  Created by Endeavour2 on 7/17/17.
+//  Copyright © 2017 SamOg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
